5e49ef0ac7c3630181f3488b2fe5d48e0a45f7a3
John Todd <jtodd2854@gmail.com>
2020-07-16 16:41:22 -0400

    generates correct atom-atom map

--- RxnSplitter.Tests/SplitterTests.cs
+++ RxnSplitter.Tests/SplitterTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 using NCDK;
 
@@ -8,6 +9,7 @@ namespace RxnSplitter
     {
         string fcSmiles = "[CH3:9][CH:8]([CH3:10])[c:7]1[cH:11][cH:12][cH:13][cH:14][cH:15]1.[CH2:3]([CH2:4][C:5](=[O:6])Cl)[CH2:2][Cl:1]>[Al+3].[Cl-].[Cl-].[Cl-].C(Cl)Cl>[CH3:9][CH:8]([CH3:10])[c:7]1[cH:11][cH:12][c:13]([cH:14][cH:15]1)[C:5](=[O:6])[CH2:4][CH2:3][CH2:2][Cl:1] |f:2.3.4.5| Friedel-Crafts acylation [3.10.1]";
         string esterSmiles = "[CH3:5][CH2:6][OH:7].[CH3:1][C:2](=[O:3])[OH:4]>[H+]>[CH3:5][CH2:6][O:7][C:2](=[O:3])[CH3:1].[OH2:4] Ethyl esterification [1.7.3]";
+        string unmappedEsterSmiles = "CCO.CC(=O)O>[H+]>CCOC(=O)C.O Ethyl esterification [1.7.3]";
         string methaneOxidation = "C>>O=C=O";
 
         [Test]
@@ -40,6 +42,38 @@ namespace RxnSplitter
             Splitter.filterInorganicsFromReaction(rxn);
             Assert.AreEqual(2, rxn.Reactants.Count);
             Assert.AreEqual(1, rxn.Products.Count);
+        }
+
+        [Test]
+        public void it_returns_empty_maps_if_no_mappings_are_present()
+        {
+            var rxn = Splitter.ParseSmiles(unmappedEsterSmiles);
+            Splitter.filterInorganicsFromReaction(rxn);
+            var map = Splitter.GetMaps(rxn);
+            Assert.IsEmpty(map);
+        }
+
+        [Test]
+        public void it_produces_correct_mappings()
+        {
+            var rxn = Splitter.ParseSmiles(esterSmiles);
+            var ethanol = rxn.Reactants[0];
+            var aceticAcid = rxn.Reactants[1];
+            var ethylAcetate = rxn.Products[0];
+
+            var expectedMap = new Dictionary<IAtom, IAtom>();
+            expectedMap.Add(aceticAcid.Atoms[0], ethylAcetate.Atoms[5]);
+            expectedMap.Add(aceticAcid.Atoms[1], ethylAcetate.Atoms[3]);
+            expectedMap.Add(aceticAcid.Atoms[2], ethylAcetate.Atoms[4]);
+            expectedMap.Add(aceticAcid.Atoms[3], null);
+            expectedMap.Add(ethanol.Atoms[0], ethylAcetate.Atoms[0]);
+            expectedMap.Add(ethanol.Atoms[1], ethylAcetate.Atoms[1]);
+            expectedMap.Add(ethanol.Atoms[2], ethylAcetate.Atoms[2]);
+
+            Splitter.filterInorganicsFromReaction(rxn);
+            var map = Splitter.GetMaps(rxn);
+            Assert.IsNotEmpty(map);
+            Assert.AreEqual(expectedMap, map);
         }
     }
 }
--- RxnSplitter/Splitter.cs
+++ RxnSplitter/Splitter.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using NCDK;
+using NCDK.Default;
 using NCDK.Tools.Manipulator;
 
 namespace RxnSplitter
@@ -30,6 +33,45 @@ namespace RxnSplitter
                     rxnIt.Remove(sub);
                 }
             }
+        }
+
+        public static IDictionary<IAtom, IAtom> GetMaps(IReaction rxn)
+        {
+            var rctMapNumbers = getMapNumbers(rxn.Reactants);
+            var prdMapNumbers = getMapNumbers(rxn.Products);
+            Dictionary<int, IMapping> atomMap = rctMapNumbers.Keys.Union(prdMapNumbers.Keys).
+                Select(n => {
+                    IAtom rctAtom, prdAtom;
+                    rctMapNumbers.TryGetValue(n, out rctAtom);
+                    prdMapNumbers.TryGetValue(n, out prdAtom);
+                    return new KeyValuePair<int, IMapping>(n, new Mapping(rctAtom, prdAtom));
+                }).
+                ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            IDictionary<IAtom, IAtom> rctMap = rctMapNumbers.
+                Select(kvp => {
+                    IMapping theMapping;
+                    IMapping outMapping;
+                    if (atomMap.TryGetValue(kvp.Key, out outMapping))
+                    {
+                        theMapping = outMapping;
+                    } else
+                    {
+                        theMapping = new Mapping(kvp.Value, null);
+                    }
+                    return new KeyValuePair<IAtom, IMapping>(kvp.Value, outMapping);
+                }).
+                Select(kvp => new KeyValuePair<IAtom, IAtom>(kvp.Key, (IAtom) kvp.Value[1])).
+                ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            return rctMap;
+        }
+
+        private static IDictionary<int, IAtom> getMapNumbers(IEnumerable<IAtomContainer> atomList)
+        {
+            return atomList.
+                SelectMany(sub => sub.Atoms).
+                Where(atom => atom.GetProperty<int>(CDKPropertyName.AtomAtomMapping, -1) != -1).
+                Select(atom => new KeyValuePair<int, IAtom>(atom.GetProperty<int>(CDKPropertyName.AtomAtomMapping), atom)).
+                ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
         }
     }
 }
