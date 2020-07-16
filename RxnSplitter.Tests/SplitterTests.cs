5974390151e02ded3f3c1daca81ef6dc59a5bba8
John Todd <jtodd2854@gmail.com>
2020-07-16 14:58:44 -0400

    filters inorganic reactants and products from reactions

--- RxnSplitter.Tests/SplitterTests.cs
+++ RxnSplitter.Tests/SplitterTests.cs
@@ -7,6 +7,7 @@ namespace RxnSplitter
     public class SplitterTests
     {
         string fcSmiles = "[CH3:9][CH:8]([CH3:10])[c:7]1[cH:11][cH:12][cH:13][cH:14][cH:15]1.[CH2:3]([CH2:4][C:5](=[O:6])Cl)[CH2:2][Cl:1]>[Al+3].[Cl-].[Cl-].[Cl-].C(Cl)Cl>[CH3:9][CH:8]([CH3:10])[c:7]1[cH:11][cH:12][c:13]([cH:14][cH:15]1)[C:5](=[O:6])[CH2:4][CH2:3][CH2:2][Cl:1] |f:2.3.4.5| Friedel-Crafts acylation [3.10.1]";
+        string esterSmiles = "[CH3:5][CH2:6][OH:7].[CH3:1][C:2](=[O:3])[OH:4]>[H+]>[CH3:5][CH2:6][O:7][C:2](=[O:3])[CH3:1].[OH2:4] Ethyl esterification [1.7.3]";
         string methaneOxidation = "C>>O=C=O";
 
         [Test]
@@ -28,6 +29,15 @@ namespace RxnSplitter
         public void it_correctly_parses_reactants_and_products()
         {
             var rxn = Splitter.ParseSmiles(fcSmiles);
+            Assert.AreEqual(2, rxn.Reactants.Count);
+            Assert.AreEqual(1, rxn.Products.Count);
+        }
+
+        [Test]
+        public void it_filters_inorganic_reactants_and_products()
+        {
+            var rxn = Splitter.ParseSmiles(esterSmiles);
+            Splitter.filterInorganicsFromReaction(rxn);
             Assert.AreEqual(2, rxn.Reactants.Count);
             Assert.AreEqual(1, rxn.Products.Count);
         }
--- RxnSplitter/Splitter.cs
+++ RxnSplitter/Splitter.cs
@@ -1,4 +1,5 @@
 using NCDK;
+using NCDK.Tools.Manipulator;
 
 namespace RxnSplitter
 {
@@ -9,6 +10,26 @@ namespace RxnSplitter
             IReaction rxn = CDK.SmilesParser.ParseReactionSmiles(smiles);
             rxn.SetProperty(CDKPropertyName.SMILES, smiles);
             return rxn;
+        }
+
+        public static void filterInorganicsFromReaction(IReaction rxn)
+        {
+            filterInorganicsFromIterator(rxn.Reactants);
+            filterInorganicsFromIterator(rxn.Products);
+            return;
+        }
+
+        private static void filterInorganicsFromIterator(IChemObjectSet<IAtomContainer> rxnIt)
+        {
+            for (int i = rxnIt.Count - 1; i >= 0; --i)
+            {
+                var sub = rxnIt[i];
+                var formula = MolecularFormulaManipulator.GetMolecularFormula(sub);
+                if (!MolecularFormulaManipulator.ContainsElement(formula, ChemicalElement.C))
+                {
+                    rxnIt.Remove(sub);
+                }
+            }
         }
     }
 }
